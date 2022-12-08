4b782844368a8fc43fa6f49ecc7f3baaa0b5a7a9
Tom <tnguyen65699@gmail.com>
2022-12-07 20:42:02 -0800

    4L with dropout

--- trainingCNN.py
+++ trainingCNN.py
@@ -65,7 +65,7 @@ def GenreModel(input_shape=(IMG_HEIGHT, IMG_WIDTH, 1), classes=16):
     X = Activation('relu')(X)
     X = MaxPooling2D((2, 2))(X)
 
-    X = Conv2D(16, kernel_size=(3, 3), strides=(1, 1))(X_input)
+    X = Conv2D(16, kernel_size=(3, 3), strides=(1, 1))(X)
     X = BatchNormalization(axis=3)(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((2, 2))(X)
@@ -86,7 +86,7 @@ def GenreModel(input_shape=(IMG_HEIGHT, IMG_WIDTH, 1), classes=16):
     # X = MaxPooling2D((2, 2))(X)
 
     X = Flatten()(X)
-    X = Dropout(rate=0.5)(X)
+    X = Dropout(rate=0.3)(X)
     X = Dense(classes, activation='softmax', name='fc' + str(classes))(X)
 
     model = Model(inputs=X_input, outputs=X, name='GenreModel')
@@ -112,7 +112,7 @@ opt = tf.keras.optimizers.Adam(learning_rate=0.0001)
 model.compile(optimizer=opt, loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_data_gen, epochs=20, validation_data=val_data_gen)
+model.fit(train_data_gen, epochs=30, validation_data=test_data_gen)
 model.save('melCNN4LDropout')
 
 model = tf.keras.models.load_model('melCNN4LDropout')
